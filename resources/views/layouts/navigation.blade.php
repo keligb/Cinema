41ae234726149908147ca564a2adc8950bae55e3
Brindeau Kelig <keligbrindeau@gmail.com>
2021-06-28 21:38:08 +0200

    base projet

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,8 +13,13 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('Programmer une séance') }}
                     </x-nav-link>
+
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                        {{ __('Voir les séances') }}
+                    </x-nav-link>
+
                 </div>
             </div>
 
